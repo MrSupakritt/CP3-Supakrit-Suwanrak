8d18e6cb3766ce44efe15471e5252dbf3f2bae8a
MrSupakritt <99985641+MrSupakritt@users.noreply.github.com>
2022-10-12 18:05:39 +0700

    Add files via upload

---
+++ Example4_Supakrtit_S.py
@@ -0,0 +1,10 @@
+FoundEng = 78.7
+GenBus = 85.9
+IntroComSys = 79.9
+ComProg = 91.0
+## display
+print("-------------------YOUR SCORE-------------------")
+print("Foundation English:","                ",FoundEng,"points")
+print("General Business:","                  ",GenBus,"points")
+print("Introduction to Computer Systems:","  ",IntroComSys,"points")
+print("Computer Programming:","              ",ComProg,"points")
